a4f4d5fd6a8e02875b740d61af2cace89672c7c7
hitobi1014 <hitobi1014@gmail.com>
2024-09-11 19:55:18 +0900

    js에서 ts 처럼 작성하기 - JS doc 이용해서 타입 지정하기 @ts-check 사용시 컴파일 과정에서 알려줌

--- typescript-basic/learn-typescript/why-ts/sample.js
+++ typescript-basic/learn-typescript/why-ts/sample.js
@@ -1,3 +1,15 @@
-function sum1(a, b) {
+// function sum1(a, b) {
+//   return a + b;
+// }
+
+// @ts-check
+/**
+ * @param {number} a 첫번째 숫자
+ * @param {number} b 두번째 숫자
+ * @returns {number}
+ */
+function sum(a, b) {
   return a + b;
 }
+
+const result = sum(10, 20);
