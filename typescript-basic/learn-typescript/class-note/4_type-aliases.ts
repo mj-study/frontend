ab98442cb6b52df418abd9478e6d31048a4ac49c
hitobi1014 <hitobi1014@gmail.com>
2024-09-12 13:15:57 +0900

    타입 별칭

---
+++ typescript-basic/learn-typescript/class-note/4_type-aliases.ts
@@ -0,0 +1,19 @@
+interface Person {
+  name: string;
+  age: number;
+}
+
+type Person2 = {
+  name: string;
+  age: number;
+}
+
+const seho: Person = {
+  name: '세호',
+  age: 30
+}
+
+const jimin: Person2 = {
+  name: "지민",
+  age: 31,
+};
